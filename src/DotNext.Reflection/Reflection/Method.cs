d88454aab1eeb86a1f6b9b2762e6258efe70a130
sakno <roman.sakno@gmail.com>
2020-11-09 22:56:21 +0200

    Fixed compiler warning

--- src/DotNext.Reflection/Reflection/Method.cs
+++ src/DotNext.Reflection/Reflection/Method.cs
@@ -484,15 +484,23 @@ namespace DotNext.Reflection
             // adjust THIS
             Expression? thisArg;
             if (thisParam is null || method.DeclaringType is null)
+            {
                 thisArg = null;
+            }
             else if (method.DeclaringType.IsAssignableFromWithoutBoxing(thisParam.Type))
+            {
                 thisArg = thisParam;
+            }
             else if (thisParam.Type == typeof(object))
+            {
                 thisArg = method.DeclaringType.IsValueType ?
                     Expression.Unbox(thisParam, method.DeclaringType) :
                     Expression.Convert(thisParam, method.DeclaringType);
+            }
             else
+            {
                 return null;
+            }
 
             // adjust arguments
             if (!Signature.NormalizeArguments(method.GetParameterTypes(), arglist, locals, prologue, epilogue))
--- src/DotNext.Reflection/Runtime/CompilerServices/CodeGenerator.cs
+++ src/DotNext.Reflection/Runtime/CompilerServices/CodeGenerator.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Linq.Expressions;
 using System.Reflection;
 using System.Reflection.Emit;
 using System.Runtime.CompilerServices;
@@ -73,30 +72,6 @@ namespace DotNext.Runtime.CompilerServices
         internal static void AsTypedReference(this ILGenerator generator, Type typeToken)
         {
             generator.Emit(OpCodes.Call, typeof(CodeGenerator).GetMethod(nameof(AsTypedReference), 1, new[] { typeof(object).MakeByRefType() }).MakeGenericMethod(typeToken));
-        }
-
-        internal static unsafe object Wrap<T>(T* ptr)
-            where T : unmanaged => Pointer.Box(ptr, typeof(T*));
-
-        internal static unsafe T* Unwrap<T>(object ptr)
-            where T : unmanaged => (T*)Pointer.Unbox(ptr);
-
-        internal static Expression Wrap(Expression expression)
-        {
-            Debug.Assert(expression.Type.IsPointer);
-            var elementType = expression.Type.GetElementType();
-            if (elementType == typeof(void))
-                return Expression.Call(typeof(Pointer), nameof(Pointer.Box), Array.Empty<Type>(), expression, Expression.Constant(typeof(void*)));
-            return Expression.Call(typeof(CodeGenerator), nameof(Wrap), new[] { elementType }, expression);
-        }
-
-        internal static Expression Unwrap(Expression expression, Type expectedType)
-        {
-            Debug.Assert(expectedType.IsPointer);
-            var elementType = expectedType.GetElementType();
-            if (elementType == typeof(void))
-                return Expression.Call(typeof(Pointer), nameof(Pointer.Unbox), Array.Empty<Type>(), expression);
-            return Expression.Call(typeof(CodeGenerator), nameof(Unwrap), new[] { elementType }, expression);
         }
     }
 }
